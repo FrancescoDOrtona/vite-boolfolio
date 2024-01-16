a9262c117aedf8c7859e69c3d7593f769c505977
FrancescoDOrtona <francescod.97@hotmail.it>
2024-01-16 17:12:23 +0100

    Add: appheader, style on header. homepage.vue style & content. App.vue components and route view

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <nav class="nav-bar">
+            <ul class="nav-bar_items">
+                <li>Home</li>
+                <li>Portfolio</li>
+                <li>Contacts</li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.nav-bar{
+    padding: 20px;
+    border-bottom: 2px solid lightblue;
+    box-shadow: 0 0 0 1 rgba(0, 0, 2,  0.300);
+}
+
+.nav-bar_items{
+    display: flex;
+    gap: 20px;
+    
+    li{
+        font-weight: bold;
+        font-size: 15px;
+    }
+}
+</style>
--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -1,15 +1,22 @@
 <template>
-    <div>
-        Homepage
+    <div class="container">
+        <div class="home-title">
+            <h1>Welcome to my collection</h1>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+
+}
 </script>
 
 <style lang="scss" scoped>
-
+.home-title{
+    margin-top: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 </style>
