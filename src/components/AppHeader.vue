56e5f2ff272d51c0f9ec1474d8f162c935b4ef9d
FrancescoDOrtona <francescod.97@hotmail.it>
2024-01-16 17:33:01 +0100

    Add: logo img

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <nav class="nav-bar">
+            <div class="logo">
+                <img src="../../public/Portfolio.hu_full_logo.png" alt="logo">
+            </div>
             <ul class="nav-bar_items">
                 <li>
                     <router-link :to="{ name:'home'}">Home</router-link>
@@ -22,9 +25,12 @@
 
 <style lang="scss" scoped>
 .nav-bar{
-    padding: 20px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 10px;
     border-bottom: 2px solid lightblue;
-    box-shadow: 0 0 0 1 rgba(0, 0, 2,  0.300);
+    box-shadow: -1px 5px 10px 2px rgba(0, 0, 2, 0.3);
 }
 
 .nav-bar_items{
@@ -35,5 +41,13 @@
         font-weight: bold;
         font-size: 15px;
     }
+}
+
+.logo{
+    align-self: flex-start;
+}
+.logo img{
+    width: 80px;
+    display: block;
 }
 </style>
